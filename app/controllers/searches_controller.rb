95f3d9a636f8160c27ee8e821527302d7e3b70a6
EC2 Default User <ec2-user@ip-172-31-33-146.ap-northeast-1.compute.internal>
2025-06-07 01:22:09 +0000

    1-5

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,5 +2,16 @@ class SearchesController < ApplicationController
   before_action :authenticate_user!
 
   def search
+    @model = params[:model]
+    @keyword = params[:keyword]
+    @search_type = params[:search_type]
     
+    if @model == "User"
+      @records = User.search(@keyword, @search_type)
+    elsif @model == "Book"
+      @records = Book.search(@keyword, @search_type)
+    else 
+      @records = []
+    end 
+  end 
 end
