668ae6a71e65c7fb3273baafd93280f1e6d80048
EC2 Default User <ec2-user@ip-172-31-33-146.ap-northeast-1.compute.internal>
2025-06-07 11:04:31 +0000

    1-6

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -2,7 +2,7 @@ class PostCommentsController < ApplicationController
 
   def create 
     book = Book.find(params[:book_id])
-    comment = current_user.comments.new(post_comment_params)
+    comment = current_user.post_comments.new(post_comment_params)
     comment.book_id = book.id 
     comment.save 
     redirect_to book_post_comments_path(book)
