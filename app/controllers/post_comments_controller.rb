83d8a4759bd452c466ab3b92da409a3acb927098
EC2 Default User <ec2-user@ip-172-31-33-146.ap-northeast-1.compute.internal>
2025-06-09 13:20:03 +0000

    1-6

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,16 +1,18 @@
 class PostCommentsController < ApplicationController
 
   def create 
-    book = Book.find(params[:book_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id 
-    comment.save 
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.post_comments.new(post_comment_params)
+    @comment.book_id = @book.id 
+    @comment.save 
   end 
 
   def destroy 
-    PostComment.find_by(id: params[:id], book_id: params[:book_id]).destroy 
-    redirect_to request.referer
+    # PostComment.find_by(id: params[:id], book_id: params[:book_id]).destroy 
+    # redirect_to request.referer
+    @comment = PostComment.find(params[:id])
+    @book = @comment.book
+    @comment.destroy
   end 
 
   private
